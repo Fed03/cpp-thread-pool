6a759d3f0722e4083f385431ace546c63444a587
Federico Teotini <templarblack.fed03@gmail.com>
2017-12-11 11:36:21 +0100

    test setup

--- main.cpp
+++ main.cpp
@@ -2,9 +2,53 @@
 #include <thread>
 #include <numeric>
 #include <future>
+#include "Executors.h"
 
-#define BUFFER_SIZE 10000
-#define NUM_THREADS 10
+#define BUFFER_SIZE 1000000
+#define CHUNK_NUMBER 100
+#define REPETITIONS 100
+
+int accumulateBlock(int *buffer, int blockSize, int blockOffset) {
+    int start = blockOffset * blockSize;
+    int end = start + blockSize;
+    return std::accumulate(buffer + start, buffer + end, 0);
+}
+
+void poolSolution(int *buffer, int blockSize) {
+    ThreadPool& pool = Executors::newAutoThreadPool();
+
+    std::future<int> futuresList[CHUNK_NUMBER];
+
+    for (int k = 0; k < CHUNK_NUMBER; ++k) {
+        futuresList[k] = pool.submit(std::bind(accumulateBlock, std::ref(buffer), blockSize, k));
+    }
+
+    int result = 0;
+    for(auto &future : futuresList) {
+        result += future.get();
+    }
+}
+
+void spawnSolution(int *buffer, int blockSize) {
+    std::future<int> futuresList[CHUNK_NUMBER];
+    std::thread threads[CHUNK_NUMBER];
+
+    for (int j = 0; j < CHUNK_NUMBER; ++j) {
+        std::packaged_task<int()> task(std::bind(accumulateBlock, std::ref(buffer), blockSize, j));
+
+        futuresList[j] = task.get_future();
+        threads[j] = std::thread(std::move(task));
+    }
+
+    int result = 0;
+    for(auto &future : futuresList) {
+        result += future.get();
+    }
+
+    for(auto &thread: threads) {
+        thread.join();
+    }
+}
 
 int main() {
     int buffer[BUFFER_SIZE];
@@ -12,34 +56,19 @@ int main() {
         buffer[i] = 1;
     }
 
-    std::future<int> futures[NUM_THREADS];
-    int blockSize = BUFFER_SIZE/NUM_THREADS;
+    int blockSize = BUFFER_SIZE/CHUNK_NUMBER;
 
-    auto start = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::microseconds(0);
+    for (long j = 0; j < REPETITIONS; ++j) {
+        auto start = std::chrono::high_resolution_clock::now();
+//        poolSolution(std::ref(buffer), blockSize);
+        spawnSolution(std::ref(buffer), blockSize);
+        auto end = std::chrono::high_resolution_clock::now();
 
-    std::thread threads[NUM_THREADS];
-    for (int j = 0; j < NUM_THREADS; ++j) {
-        std::packaged_task<int()> task([&buffer, j, blockSize] {
-            int start = j*blockSize;
-            int end = start + blockSize;
-            return std::accumulate(buffer + start, buffer + end, 0);
-        });
-
-        futures[j] = task.get_future();
-        threads[j] = std::thread(std::move(task));
+        duration += std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     }
 
-    int result = 0;
-    for(auto &future : futures) {
-        result += future.get();
-    }
-    auto end = std::chrono::high_resolution_clock::now();
+    auto avg = duration.count() / REPETITIONS;
 
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end- start).count();
-    std::cout<< duration << std::endl;
-
-    for(auto &thread: threads) {
-        thread.join();
-    }
-//    ThreadPool& pool = Executors::newAutoThreadPool();
+    std::cout << avg << std::endl;
 }
